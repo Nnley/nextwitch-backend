bbf401542ee1ed8eb4eb8a885cf68528a6deb19a
Nnley <kragelek86@gmail.com>
2025-02-22 02:57:29 +0300

    Updated CronModule with additional service dependencies

--- src/modules/cron/cron.module.ts
+++ src/modules/cron/cron.module.ts
@@ -1,9 +1,13 @@
+import { PrismaService } from '@/src/core/prisma/prisma.service'
 import { Module } from '@nestjs/common'
 import { ScheduleModule } from '@nestjs/schedule'
+import { StorageService } from '../libs/storage/storage.service'
+import { TelegramService } from '../libs/telegram/telegram.service'
+import { NotificationService } from '../notification/notification.service'
 import { CronService } from './cron.service'
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  providers: [CronService],
+  providers: [CronService, PrismaService, StorageService, TelegramService, NotificationService],
 })
 export class CronModule {}
